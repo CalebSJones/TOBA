ce3cb7bda8cfbf6bda9b89325efde45a0188e972
Caleb Jones <Caleb Jones@DESKTOP-F1J7TTP>
2017-03-29 22:29:31 -0400

    Caleb Jones Week 2 Module 1 Assignment

---
+++ src/java/NewCustomerServlet.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ *
+ * @author Caleb Jones
+ */
+@WebServlet(name="NewCustomerServlet", urlPatterns={"/NewCustomerServlet"})
+public class NewCustomerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String url = "";
+        
+        // get paramters from the request
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String phone = request.getParameter("phone");
+        String address = request.getParameter("address");
+        String city = request.getParameter("city");
+        String state = request.getParameter("state");
+        String zipcode = request.getParameter("zip");
+        String email = request.getParameter("email");
+
+        // Validate the parameters.
+        String message;
+        if (username == null || password == null || phone == null || address == null
+            || city == null || state == null || zipcode == null || email == null
+            || username.isEmpty() || password.isEmpty() || phone.isEmpty()
+            || address.isEmpty() || city.isEmpty() || state.isEmpty()
+            || zipcode.isEmpty() || email.isEmpty()) {
+            
+            message = "Please fill out all the form fields.";
+            url = "New_customer.html";
+        } else {
+            // Get a relative file name
+            ServletContext sc = getServletContext();
+            String path = sc.getRealPath("/WEB-INF/customers.txt");
+            url = "Success.html";
+        }
+        response.sendRedirect(url);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/Error_java.jsp
@@ -0,0 +1,23 @@
+I ac<%-- 
+    Document   : Error_java
+    Created on : Mar 27, 2017, 11:21:20 PM
+    Author     : Caleb Jones
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Java Error Page</title>
+    </head>
+    <body>
+        <h1>Java Error</h1>
+        <p>Sorry, Java has thrown an exception.</p>
+        <p>To continue, click the Back button.</p>
+        
+        <h2>Details</h2>
+        <p>Type: {pageContext.exception["class"]}</p>
+        <p>Message: {pageContext.exception.message}</p>
+    </body>
+</html>
